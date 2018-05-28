6ef434288021755b2f289daac7379a1c0994cf97
Nicolas Mahe <nicolas@mahe.me>
2018-05-28 16:30:58 +0700

    Connect to daemon by using the TCP connection.

--- index.js
+++ index.js
@@ -4,33 +4,27 @@ const fs = require('fs')
 const vm = require('vm')
 const proto = grpc.load('./proto/api.proto')
 const mesg = new proto.api.Service(
-  process.env.MESG_ENDPOINT,
+  process.env.MESG_ENDPOINT_TCP,
   grpc.credentials.createInsecure(),
-  {}
 )
 const service = yaml.safeLoad(fs.readFileSync("./mesg.yml"))
-
-console.log("1")
-
 const listener = mesg.ListenTask({ service })
-
-console.log(listener)
-
+console.log("Start ListenTask")
 listener
   .on('error', console.log)
   .on('end', console.log)
   .on('status', console.log)
   .on('data', ({ executionID, taskKey, inputData }) => {
-    console.log("3")
-    if (taskKey !== "onExecution") { throw new Error("Task doesn't exists") }
-    console.log("4")
-    console.log(inputs)
-    func = vm.runInContext(inputData.code, vm.createContext({ module, console, require }))
-    const res = func(inputData.inputs)
-  
-    serviceApi.SubmitResult({
+    if (taskKey !== "execute") { throw new Error("Task doesn't exists") }
+    // console.log(inputData)
+    // const sandbox = { module, console, require }
+    // vm.createContext(sandbox)
+    // vm.runInContext(inputData.code, sandbox)
+    // // console.log('sandbox', sandbox)
+    // const res = sandbox
+    mesg.SubmitResult({
       executionID,
       outputKey: "result",
-      outputData: res
+      outputData: JSON.stringify({"foo":"bar"})
     }, console.log)
   })
