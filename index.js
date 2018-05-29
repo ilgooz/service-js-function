12190c6aeba549d005ef56b34a5916d7e4e596d0
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-29 16:10:55 -0700

    make js function works

--- index.js
+++ index.js
@@ -10,21 +10,32 @@ const mesg = new proto.api.Service(
 const service = yaml.safeLoad(fs.readFileSync("./mesg.yml"))
 const listener = mesg.ListenTask({ service })
 console.log("Start ListenTask")
-listener
-  .on('error', console.log)
-  .on('end', console.log)
-  .on('status', console.log)
-  .on('data', ({ executionID, taskKey, inputData }) => {
-    if (taskKey !== "execute") { throw new Error("Task doesn't exists") }
-    // console.log(inputData)
-    // const sandbox = { module, console, require }
-    // vm.createContext(sandbox)
-    // vm.runInContext(inputData.code, sandbox)
-    // // console.log('sandbox', sandbox)
-    // const res = sandbox
-    mesg.SubmitResult({
-      executionID,
-      outputKey: "result",
-      outputData: JSON.stringify({"foo":"bar"})
-    }, console.log)
-  })
+
+const execute = inputs => ({
+  output: "result",
+  data: vm
+    .runInContext(inputs.code, vm.createContext({
+      module,
+      console,
+      require
+    }))(inputs.input) || null
+})
+
+listener.on('data', ({ executionID, taskKey, inputData }) => {
+  const inputs = JSON.parse(inputData)
+  let result = null
+  switch (taskKey) {
+    case "execute":
+      result = execute(inputs)
+      break;
+    default:
+      throw new Error("Task doesn't exists")
+      break;
+  }
+  console.log(result)
+  mesg.SubmitResult({
+    executionID,
+    outputKey: result.output,
+    outputData: JSON.stringify(result.data)
+  }, console.log)
+})
