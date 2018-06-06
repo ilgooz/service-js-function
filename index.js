78e88db9e6e12ac46190e56d5baed5d2ef82db6e
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-06 11:30:11 -0700

    merge

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
-const grpc = require('grpc')
-const yaml = require('js-yaml')
-const fs = require('fs')
 const vm = require('vm')
-const proto = grpc.load('./proto/api.proto')
-const mesg = new proto.api.Service(
-  process.env.MESG_ENDPOINT_TCP,
-  grpc.credentials.createInsecure(),
-)
-const service = yaml.safeLoad(fs.readFileSync("./mesg.yml"))
-const listener = mesg.ListenTask({ service })
-console.log("Start ListenTask")
+const MESG = require("mesg-js").service
 
-const execute = inputs => ({
-  output: "result",
-  data: vm
-    .runInContext(inputs.code, vm.createContext({
-      module,
-      console,
-      require
-    }))(inputs.inputs) || null
-})
+const execute = ({ inputs, code }, { result }) => {
+  const func = vm.runInContext(code, vm.createContext({ module, console, require }))
+  result(func(inputs))
+}
 
-listener.on('data', ({ executionID, taskKey, inputData }) => {
-  const inputs = JSON.parse(inputData)
-  let result = null
-  switch (taskKey) {
-    case "execute":
-      result = execute(inputs)
-      break;
-    default:
-      throw new Error("Task doesn't exists")
-      break;
-  }
-  console.log(result)
-  mesg.SubmitResult({
-    executionID,
-    outputKey: result.output,
-    outputData: JSON.stringify(result.data)
-  }, console.log)
-})
+MESG.listenTask({ execute })
