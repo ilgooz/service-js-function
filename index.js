39e4a41bef9c157f37beed75bac2992c22f8bcbb
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-29 17:46:38 +0700

    update library and add mesgignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const vm = require('vm')
-const MESG = require("mesg-js").service
+const MESG = require("mesg-js").service()
 
 const execute = ({ inputs, code }, { result }) => {
   const func = vm.runInContext(code, vm.createContext({ module, console, require }))
